cd45b0ad4f60d0cf0e6919c9eece1d299669f214
Maren McMullan <marenmc@gmail.com>
2017-09-30 12:34:02 -0700

    Changes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'user_jobs/index'
-
-  get 'user_jobs/show'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     resources :users, only: [:show]
     resources :jobs, only: [:index, :show]
+    resources :user_jobs, only: [:index, :show]
 
 
 
