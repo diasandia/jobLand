6b39903b5c575685ce3428828bf59a4f2ee7db71
Maren McMullan <marenmc@gmail.com>
2017-09-30 19:15:07 -0700

    Add user_job changes

--- app/controllers/user_jobs_controller.rb
+++ app/controllers/user_jobs_controller.rb
@@ -1,6 +1,12 @@
 class UserJobsController < ApplicationController
+
+  def new
+    @user_job = UserJob.new
+  end
+
   def index
-    @user_job = User_job.all
+    @user = User.find(params[:id])
+    @user_jobs = UserJob.all
   end
 
   def show
--- app/models/user_job.rb
+++ app/models/user_job.rb
@@ -5,8 +5,6 @@ class UserJob < ApplicationRecord
   has_many :events, as: :attendable
   has_many :notes, as: :notable
 
-  def jobs_by_phase
-    @user_job = User_job.all
-  end
+
 
 end
