0480dc87ffba1cab21e0ef87178d4b0d19ed27b3
Maren McMullan <marenmc@gmail.com>
2017-10-02 10:40:15 -0700

    Prune some of our routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,22 @@
 Rails.application.routes.draw do
 
-  get 'notes/create'
-
-  get 'notes/update'
-
-  get 'notes/delete'
-
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "user_jobs#index"
 
-  resources :jobs
-  resources :phases
-  resources :networking_events
-
-
   resources :users, only: [:new, :create, :show] do
-    resources :user_points
-    resources :user_jobs
-    resources :user_networking_events
+    resources :user_jobs, only: [:new, :create, :show]
   end
 
-  resources :user_jobs do
-    resources :jobs
-    resources :job_phases
-    resources :notes, only: [:create, :update, :destroy]
-    resources :events
+  resources :user_jobs, only: [:new, :create, :show] do
+    resources :notes, only: [:new, :create, :update, :destroy]
+    resources :events, only: [:new, :create, :show]
   end
 
-  resources :job_phases do
-    resources :phases
-    resources :notes, only: [:create, :update, :destroy]
-    resources :events
-  end
-
-  resources :user_networking_events do
-    resources :notes, only: [:create, :update, :destroy]
-    resources :events
-  end
+  # resources :user_networking_events do
+  #   resources :notes, only: [:create, :update, :destroy]
+  #   resources :events
+  # end
 
 end
