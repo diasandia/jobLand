e2fdbd83dbd92658461979e463b1707107de2500
Maren McMullan <marenmc@gmail.com>
2017-10-02 23:05:43 -0700

    Get rid of default values for the user scorecard thingie

--- db/migrate/20171003024505_create_rubrics.rb
+++ db/migrate/20171003024505_create_rubrics.rb
@@ -1,9 +1,9 @@
 class CreateRubrics < ActiveRecord::Migration[5.1]
   def change
     create_table :rubrics do |t|
-      t.string :standard_one, default: "Experienced Manager"
-      t.string :standard_two, default: "Location"
-      t.string :standard_three, default: "Diversity & Inclusion"
+      t.string :standard_one
+      t.string :standard_two
+      t.string :standard_three
       t.references :user
 
       t.timestamps
