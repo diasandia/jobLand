a03b96df949a6e6ea21fab6d5d853a5219ce83e6
Diana <diasandia@users.noreply.github.com>
2017-10-02 19:31:19 -0700

    Redo point category migration, model and seed - don't delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001221015) do
+ActiveRecord::Schema.define(version: 20171003015434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,13 @@ ActiveRecord::Schema.define(version: 20171001221015) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["notable_type", "notable_id"], name: "index_notes_on_notable_type_and_notable_id"
+  end
+
+  create_table "point_categories", force: :cascade do |t|
+    t.string "category"
+    t.integer "number_of_points"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_events", force: :cascade do |t|
