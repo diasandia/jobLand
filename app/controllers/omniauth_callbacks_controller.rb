dd2640133f68a3fc16d32a8905203d2ebbcf8aeb
Jenna Ritten <jritten@umich.edu>
2017-10-03 14:31:01 -0700

    removed devise sign-up/login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,16 +4,9 @@ class OmniauthCallbacksController  < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     # if @user.persisted?
-    # p "*" * 100
     if @user
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-      # if @user.criteria_one == nil
-      #   sign_in @user, event: :authentication
-      #   p "Inside the mothafuckin thingie"
-      #   render "users/criteria_form"
-      # else
       sign_in_and_redirect @user, event: :authentication
-    # end
     end
     # else
     #   session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
