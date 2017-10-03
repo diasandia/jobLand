764e08401b0b5cff3cbe703ecb914d001375b52c
Maren McMullan <marenmc@gmail.com>
2017-10-02 17:25:18 -0700

    Update user index and show pages

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,18 +1,24 @@
 class JobsController < ApplicationController
 
  def new
-    @user_job = UserJob.new
+    @job = Job.new
   end
 
   def index
     #change this to current user
-    @test_user = User.first
-    @jobs = @test_user.jobs.all
+    if current_user
+      @jobs = current_user.jobs.all
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def show
+    p "*" * 100
+    p params
     @user = current_user
-    @job = @user.jobs.find(params[:id])
+    @job = current_user.jobs.find(params[:id])
+    p @job
     @note = Note.new
   end
 
