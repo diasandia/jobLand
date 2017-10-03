ed977248fe48819b429b19e362ba0292726206ba
Jenna Ritten <jritten@umich.edu>
2017-10-03 11:42:35 -0700

    oauth is functional; logs into user show/root

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -7,19 +7,18 @@ class OmniauthCallbacksController  < Devise::OmniauthCallbacksController
     # p "*" * 100
     if @user
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-      if @user.criteria_one == nil
-        sign_in @user, event: :authentication
-        p "Inside the mothafuckin thingie"
-        render "users/criteria_form"
-      else
-        sign_in_and_redirect @user, event: :authentication
-      end
+      # if @user.criteria_one == nil
+      #   sign_in @user, event: :authentication
+      #   p "Inside the mothafuckin thingie"
+      #   render "users/criteria_form"
+      # else
+      sign_in_and_redirect @user, event: :authentication
+    # end
     end
     # else
     #   session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
     #   # redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     #   render "devise/registrations/criteria_form"
     # end
-
   end
 end
