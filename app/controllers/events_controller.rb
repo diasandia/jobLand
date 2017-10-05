1c8cbe4e689c50e6d70700875c8138b97959cbed
Maren McMullan <marenmc@gmail.com>
2017-10-05 11:21:33 -0700

    shorten the dropdown for the current job phase

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,25 @@
 class EventsController < ApplicationController
+  def index
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @job = Job.find(params[:job_id])
+
+    if params[:job_id]
+      @job = Job.find(params[:job_id])
+    elsif params[:job_phase_id]
+      @job_phase_id = JobPhase.find(:job_phase_id)
+    end
+
+    @event = @job.events.new
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :location, :datetime)
 end
